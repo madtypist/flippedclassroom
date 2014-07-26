379baa1448724ec2b7fc00a0859beb9d66250bfc
madtypist <mad_typist@yahoo.com>
2014-07-26 15:59:51 -0400

    Add some bootstrap styling. More tests to pull the right data from the YouTube playlists

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -14,6 +14,10 @@ class StaticPagesController < ApplicationController
     data = response.body
     @results = JSON.parse(data)
 
+    plResponse = Net::HTTP.get_response(URI.parse("https://www.googleapis.com/youtube/v3/playlists?part=snippet&id=PLDOix9BwbbieH5xnN0lDTkwNVULQ14Usc&key=AIzaSyCPK2qXfkJ2LYAydvWgVWl3DuKosC_CjNA"))
+    data2 = plResponse.body
+    @listinfo = JSON.parse(data2)
+    #TODO: Retreive playlist title
     @items = @results["items"]
   end
 end
